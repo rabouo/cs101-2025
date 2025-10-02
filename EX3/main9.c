f52c53a4e7af258665f8a67b23fe76a722eee8a1
rab_ouo <125680692+rabouo@users.noreply.github.com>
2025-10-02 14:46:15 +0800

    Create main9.c

---
+++ EX3/main9.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int x = -9;
+    int y = 9;
+    int z = 1;
+    int sum;
+    if(x<0){
+        sum = 100*x*-1+y*10+z;  
+        sum *= -1;
+    }else{
+        sum = 100*x+10*y+z;
+    }
+    printf("%d",sum);
+}
