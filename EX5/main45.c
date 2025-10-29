decc92d90f06cbd2d221276bbd47d75335b64581
rab_ouo <125680692+rabouo@users.noreply.github.com>
2025-10-30 07:59:43 +0800

    Update main45.c

--- EX5/main45.c
+++ EX5/main45.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 void towerOfHanoi(int n, char from_rod, char to_rod, char aux_rod) {
   if (n == 1) {
-    printf("\n Move disk 1 from rod %c to rod %c , from_rod, to_rod);
+    printf("\n Move disk 1 from rod %c to rod %c", from_rod, to_rod);
     return;
   }
   towerOfHanoi(n-1, from_rod, aux_rod, to_rod);
   printf("\n Move disk %d from rod %c to rod %c", n, from_rod, to_rod);
-  tower0fHanoi(n-1, aux_rod, to_rod, from_rod);
+  towerOfHanoi(n-1, aux_rod, to_rod, from_rod);
 }
 
 int main() {
   int n = 3;
-  tower0fHanoi(n, 'A', 'B', 'C');
+  towerOfHanoi(n, 'A', 'B', 'C');
   return 0;
 }
